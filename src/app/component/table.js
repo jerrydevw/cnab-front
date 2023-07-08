e30327427381de2d4a630113f802cce344ccfe05
Jerry Martins <jerry.devw@gmail.com>
2023-07-08 16:57:00 -0400

    upload e busca de canb

---
+++ src/app/component/table.js
@@ -0,0 +1,44 @@
+import Table from 'react-bootstrap/Table';
+
+function TableCnab(cnabsPaginated) {
+    console.log(cnabsPaginated.cnabsPaginated?.content);
+    
+    const tableHeaders = [
+        "Data", "Valor", "Cpf", "Numero do cartao", "Hora", "Dono da loja", "Nome da loja", "Tipo", "Natureza"
+    ]
+
+
+    return (
+        <Table striped bordered hover>
+
+            <thead>
+                <tr>
+                    <th>#</th>
+                    {tableHeaders.map((headerName, index) => (
+                        <th key={index}>{headerName}</th>
+                    ))}
+                </tr>
+            </thead>
+
+            <tbody>
+
+                {cnabsPaginated.cnabsPaginated?.content.map((cnab, index) => (
+                    <tr key={index}>
+                        <td>{index}</td>
+                        <td>{cnab.date}</td>
+                        <td>{cnab.value}</td>
+                        <td>{cnab.cpf}</td>
+                        <td>{cnab.cardNumber}</td>
+                        <td>{cnab.hour}</td>
+                        <td>{cnab.onwnerStore}</td>
+                        <td>{cnab.nameStore}</td>
+                        <td>{cnab.type.description}</td>
+                        <td>{cnab.type.nature}</td>
+                    </tr>    
+                ))}
+            </tbody>
+        </Table>
+    );
+}
+
+export default TableCnab;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,45 @@
 'use client'
 
 import styles from './page.module.css'
-import { ChangeEvent, useState } from 'react';
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import TableCnab from './component/table';
+
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export default function Home() {
+async function getCnabs(page, size, storeName) {
+  const params = {
+    page: page,
+    size: size,
+    storeName: storeName
+  }
 
+  const result = await axios.get('http://localhost:8080/cnab', {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    params: params
+  });
+
+  return result.data;
+
+}
+
+export default function Home() {
   const [file, setFile] = useState();
+
+  const [cnabs, setCnabs] = useState(null);
 
   const handleFileChange = (e) => {
     if (e.target.files) {
@@ -28,7 +61,7 @@ export default function Home() {
     axios.post('http://localhost:8080/cnab', formData, {
       headers: {
         "Content-Type": "multipart/form-data",
-        "Origin": "http://localhost:3000",
+        "Access-Control-Allow-Origin": "*"
       },
     })
     .then(function (response) {
@@ -37,19 +70,41 @@ export default function Home() {
     .catch(function (error) {
       console.error(error);
     });
+
   };
 
+  useEffect(() => {
+    if (cnabs == null) {
+      getCnabs(0, 2, "BAR DO JOÃO")
+      .then((result) => {
+        setCnabs(result);
+      })
+    }
+    
+  }), [cnabs];
+
   return (
-    <main className={styles.main}>
 
-      <div>
-        <form method="post" onSubmit={handleSubmit}>
-          <input type="file" onChange={handleFileChange} />
-          <div>{file && `${file.name} - ${file.type}`}</div>
-          <button type="submit">Upload</button>
-        </form>
-      </div>
+    <Container>
+      <Row>
+        <Col>
+          <>
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Control type="file" onChange={handleFileChange}/>
+            </Form.Group>
+          </>   
+        </Col>
 
-    </main>
+        <Col>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>Upload</Button>
+        </Col>
+        
+      </Row>
+
+      <Row>
+        <TableCnab cnabsPaginated={cnabs}></TableCnab>
+      </Row>
+    </Container>
+
   )
 }
