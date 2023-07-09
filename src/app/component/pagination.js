98da80d37301ff14f3b62a807fcd203851e3d804
Jerry Martins <jerry.devw@gmail.com>
2023-07-08 21:06:32 -0400

    remove console.log

--- src/app/component/pagination.js
+++ src/app/component/pagination.js
@@ -3,14 +3,11 @@ import Pagination from 'react-bootstrap/Pagination';
 import { useEffect, useState } from 'react';
 
 function goToPage(page, setPage) {
-    console.log("new page: ", page);
     setPage(page);
 }
 
 function renderItem(startIndex, quantity, current, hookSetPage) {
     let number = 0;
-
-    console.log("current: ", current);
 
     const items = [...Array(quantity).keys()].map((number) => {
         return <Pagination.Item key={number} active={number == current} onClick={() => goToPage(number, hookSetPage)}>
