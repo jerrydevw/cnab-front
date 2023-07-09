0df1a8a971fba069c80e82c674e1c2bb0e974b6e
Jerry Martins <jerry.devw@gmail.com>
2023-07-08 21:29:39 -0400

    paginacao funcionando corretamente

--- src/app/component/pagination.js
+++ src/app/component/pagination.js
@@ -1,37 +1,82 @@
-import Pagination from 'react-bootstrap/Pagination';
-
 import { useEffect, useState } from 'react';
-
-function goToPage(page, setPage) {
-    setPage(page);
-}
-
-function renderItem(startIndex, quantity, current, hookSetPage) {
-    let number = 0;
-
-    const items = [...Array(quantity).keys()].map((number) => {
-        return <Pagination.Item key={number} active={number == current} onClick={() => goToPage(number, hookSetPage)}>
-            {number + 1}
-        </Pagination.Item>
-    });
-
-    return items;
-}
+import Pagination from 'react-bootstrap/Pagination';
 
 function PaginationCustom({ setPage, currentPage, totalPages }) {
     const quantity = 3;
+    const [pageNumbers, setPageNumbers] = useState([]);
 
-    const items = renderItem(0, quantity, currentPage, setPage);
+    useEffect(() => {
+        const generatePageNumbers = () => {
+            const numbers = [];
+            for (let i = 1; i <= totalPages; i++) {
+                numbers.push(i);
+            }
+            return numbers;
+        };
+
+        setPageNumbers(generatePageNumbers());
+    }, [totalPages]);
+
+    const goToPage = (page) => {
+        setPage(page);
+    };
+
+    const goToFirstPage = () => {
+        goToPage(0);
+    };
+
+    const goToPrevPage = () => {
+        if (currentPage > 0) {
+            goToPage(currentPage - 1);
+        }
+    };
+
+    const goToNextPage = () => {
+        if (currentPage < totalPages - 1) {
+            goToPage(currentPage + 1);
+        }
+    };
+
+    const goToLastPage = () => {
+        goToPage(totalPages - 1);
+    };
+
+    const renderPageNumbers = () => {
+        let start = currentPage - Math.floor(quantity / 2);
+        if (start < 0) {
+            start = 0;
+        }
+        let end = start + quantity;
+        if (end > totalPages) {
+            end = totalPages;
+            start = end - quantity;
+            if (start < 0) {
+                start = 0;
+            }
+        }
+
+        const pageRange = pageNumbers.slice(start, end);
+
+        return pageRange.map((number) => (
+            <Pagination.Item
+                key={number}
+                active={number - 1 === currentPage}
+                onClick={() => goToPage(number - 1)}
+            >
+                {number}
+            </Pagination.Item>
+        ));
+    };
 
     return (
         <Pagination>
-            <Pagination.First onClick={() => goToPage(0, setPage)} />
-            <Pagination.Prev onClick={() => goToPage(currentPage - 1, setPage)} />
-            
-            {items}
+            <Pagination.First onClick={goToFirstPage} disabled={currentPage === 0} />
+            <Pagination.Prev onClick={goToPrevPage} disabled={currentPage === 0} />
 
-            <Pagination.Next onClick={() => goToPage(currentPage + 1, setPage)} />
-            <Pagination.Last />
+            {renderPageNumbers()}
+
+            <Pagination.Next onClick={goToNextPage} disabled={currentPage === totalPages - 1} />
+            <Pagination.Last onClick={goToLastPage} disabled={currentPage === totalPages - 1} />
         </Pagination>
     );
 }
